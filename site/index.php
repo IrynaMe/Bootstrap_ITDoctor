43ea2b9183ad2b36f5b64dd72ff927f2ffbc6028
IrynaMe <irinam7a@gmail.com>
2022-11-05 15:35:15 +0100

    Divided index into index php and about php

--- site/index.php
+++ site/index.php
@@ -57,7 +57,7 @@
    <header>
         <!-- bootstrap classes -->
         <!-- comment -->
-         <!-- comment -->
+        
         <div class="container">
             <div class="row">
                 <div class="col-12">
@@ -70,55 +70,10 @@
             </div>
         </div>
    </header>
-    
-  <section class="about">
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <h2 class="text-center text-uppercase color1 mb-5">О курсе</h2>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xl-4 col-md-6 col-sm-12">
-          <div class="itd_circle"><i class="icon-list-numbered"></i></div>
-          <h5 class="text-center">Структурированный материал</h5>
-          <div class="line"></div>
-        </div>
+   <?php
+   include('about.html');
+   ?> 
 
-        <div class="col-xl-4 col-md-6 col-sm-12">
-          <div class="itd_circle"><i class="icon-rouble"></i></div>
-          <h5 class="text-center">Приемлемая цена</h5>
-          <div class="line"></div>
-        </div>
-
-        <div class="col-xl-4 col-md-6 col-sm-12">
-          <div class="itd_circle"><i class="icon-chart-bar"></i></div>
-          <h5 class="text-center">Пожизненный доступ</h5>
-          <div class="line"></div>
-        </div>
-
-        <div class="col-xl-4 col-md-6 col-sm-12">
-          <div class="itd_circle"><i class="icon-code"></i></div>
-          <h5 class="text-center">Практические задания</h5>
-          <div class="line"></div>
-        </div>
-
-        <div class="col-xl-4 col-md-6 col-sm-12">
-          <div class="itd_circle"><i class="icon-file-video"></i></div>
-          <h5 class="text-center">35 видео уроков</h5>
-          <div class="line"></div>
-        </div>
-
-        <div class="col-xl-4 col-md-6 col-sm-12">
-          <div class="itd_circle"><i class="icon-mobile"></i></div>
-          <h5 class="text-center">Просмотр со смартфона</h5>
-          <div class="line"></div>
-        </div>
-
-      </div>
-    </div>
-
-   </section>
 
    <section class="forwhom">
     <div class="container">
